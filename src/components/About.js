86d8e4c514bb380ed2ad02b17f7f6a61f15dc903
Sourabh <devsourabhagg@gmail.com>
2024-06-09 10:29:08 +0530

    use of useEffect and setTimeout functions

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,16 @@
-import React,{useContext} from 'react'
+import React,{useContext, useEffect } from 'react'
 import noteContext from '../context/notes/noteContext'
 
 const About = () => {
 
   const a = useContext(noteContext);
+  useEffect (()=>{
+     a.update();
+     // eslint-disable-next-line
+  },[])
   return (
     <div>
-        this is about {a.class}
+        this is about {a.state.name} and he is in class {a.state.class}
     </div>
   )
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React,{useState} from 'react';
 import NoteContext from './noteContext';
 
 const NoteState = (props) => {
-    const state = {
+    const s1 = {
         "name": 'Sourabh',
         "class": '5A'
     };
 
+    const [state,setState] = useState(s1);
+    const update = () => {
+        setTimeout(()=>{
+            setState({
+                "name" : "pintu",
+                "class" : "10A"
+            })
+        },1000);
+
+    }
+
     return (
-        <NoteContext.Provider value={state}>
+        <NoteContext.Provider value={{state,update}}>
             {props.children}
         </NoteContext.Provider>
     );
