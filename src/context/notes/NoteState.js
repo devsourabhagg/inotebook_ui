6a1f3679b6ea8aa6d3e4195f43c4d310b4a8ab9d
Sourabh <devsourabhagg@gmail.com>
2024-06-15 15:51:10 +0530

    more change done

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,24 +2,65 @@ import React,{useState} from 'react';
 import NoteContext from './noteContext';
 
 const NoteState = (props) => {
-    const s1 = {
-        "name": 'Sourabh',
-        "class": '5A'
-    };
-
-    const [state,setState] = useState(s1);
-    const update = () => {
-        setTimeout(()=>{
-            setState({
-                "name" : "pintu",
-                "class" : "10A"
-            })
-        },1000);
-
-    }
-
+    const notesInitial = [
+            {
+              "_id": "6665929c9c17fd9cdb6e98b3",
+              "user": "6665925e9c17fd9cdb6e98ae",
+              "title": "Java Script",
+              "description": "This is complete MERN Stack code",
+              "tag": "Study",
+              "date": "2024-06-09T11:31:40.164Z",
+              "__v": 0
+            },
+            {
+              "_id": "666592a69c17fd9cdb6e98b5",
+              "user": "6665925e9c17fd9cdb6e98ae",
+              "title": "Java Script1",
+              "description": "This is complete MERN Stack code2",
+              "tag": "Study1",
+              "date": "2024-06-09T11:31:50.399Z",
+              "__v": 0
+            },
+            {
+                "_id": "666592a69c17fd9cdb6e98b5",
+                "user": "6665925e9c17fd9cdb6e98ae",
+                "title": "Java Script1",
+                "description": "This is complete MERN Stack code2",
+                "tag": "Study1",
+                "date": "2024-06-09T11:31:50.399Z",
+                "__v": 0
+              },
+              {
+                "_id": "666592a69c17fd9cdb6e98b5",
+                "user": "6665925e9c17fd9cdb6e98ae",
+                "title": "Java Script1",
+                "description": "This is complete MERN Stack code2",
+                "tag": "Study1",
+                "date": "2024-06-09T11:31:50.399Z",
+                "__v": 0
+              },
+              {
+                "_id": "666592a69c17fd9cdb6e98b5",
+                "user": "6665925e9c17fd9cdb6e98ae",
+                "title": "Java Script1",
+                "description": "This is complete MERN Stack code2",
+                "tag": "Study1",
+                "date": "2024-06-09T11:31:50.399Z",
+                "__v": 0
+              },
+              {
+                "_id": "666592a69c17fd9cdb6e98b5",
+                "user": "6665925e9c17fd9cdb6e98ae",
+                "title": "Java Script1",
+                "description": "This is complete MERN Stack code2",
+                "tag": "Study1",
+                "date": "2024-06-09T11:31:50.399Z",
+                "__v": 0
+              }
+          ]
+    const [notes,setNotes] = useState(notesInitial)
     return (
-        <NoteContext.Provider value={{state,update}}>
+        <NoteContext.Provider value={{notes,setNotes}}>
             {props.children}
         </NoteContext.Provider>
     );
